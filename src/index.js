1120b1f4543f79a636b9e50928462d487c4a89aa
Chris Bennett <chrisbennett09@gmail.com>
2019-09-19 16:48:16 -0500

    add taskSchema and timestamp to task.js

--- src/index.js
+++ src/index.js
@@ -26,17 +26,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
+// const Task = require('./models/task')
+// const User = require('./models/user')
 
-const main = async () => {
-    // const task = await Task.findById('5d82bfeb8133e6037cd1f673')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
+// const main = async () => {
+//     // const task = await Task.findById('5d82bfeb8133e6037cd1f673')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
 
-    const user = await User.findById('5d82bdd51e2d380348b5c940')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
+//     const user = await User.findById('5d82bdd51e2d380348b5c940')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
 
-main()
+// main()
